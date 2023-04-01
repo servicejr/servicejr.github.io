8fc0888241b4374c8cc461e9d74dbde0cd6121f4
Travis <travis@jrresources.com>
2023-03-31 21:04:06 -0700

    update

--- basichtml.js
+++ basichtml.js
@@ -2,8 +2,12 @@ $(document).ready(function(){
 	// HTML markup implementation, overlap mode
 	$( '#menu' ).multilevelpushmenu({
 		containersToPush: [$( '#pushobj' )],
-		collapsed: true
-	});
+		collapsed: true,
+		menuWidth: 450, // '450px', '30em', '25%' will also work
+        menuHeight: 500,
+		// Just for fun also changing the look of the menu
+		wrapperClass: 'mlpm_w',
+		menuInactiveClass: 'mlpm_inactive'
+
 });
-
-
+});
