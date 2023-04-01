d7c420833c229d4eb57eef01e04c4b34de51230a
Travis <travis@jrresources.com>
2023-03-31 21:24:48 -0700

    update

--- basichtml.js
+++ basichtml.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
 	$( '#menu' ).multilevelpushmenu({
 		containersToPush: [$( '#pushobj' )],
 		collapsed: true,
-		menuWidth: 450, // '450px', '30em', '25%' will also work
-        menuHeight: 500,
+	//	menuWidth: 450, // '450px', '30em', '25%' will also work
+        menuHeight: 800,
 		// Just for fun also changing the look of the menu
 		wrapperClass: 'mlpm_w',
 		menuInactiveClass: 'mlpm_inactive'
